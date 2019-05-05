26b56b2647b92d16a0c13f27fdf27168661bdc81
Victor <victorcsv@gmail.com>
2019-05-05 15:12:09 -0300

    Enviando formulário

--- index.js
+++ index.js
@@ -8,10 +8,22 @@ nunjucks.configure("views", {
   watch: true
 });
 
+app.use(express.urlencoded({ extended: false }));
 app.set("view engine", "njk");
 
+const users = ["Victor", "Camila", "Paula"];
+
 app.get("/", (req, res) => {
-  return res.render("list", { name: req.query.name });
+  return res.render("list", { users });
+});
+
+app.get("/new", (req, res) => {
+  return res.render("new");
+});
+
+app.post("/create", (req, res) => {
+  users.push(req.body.user);
+  return res.redirect("/");
 });
 
 app.listen(3000);
