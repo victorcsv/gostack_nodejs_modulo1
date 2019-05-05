fa95eba4469c05da22f5ea13311c0a8f6fe75297
Victor <victorcsv@gmail.com>
2019-05-05 15:01:26 -0300

    Adiciona e configura nunjucks

--- index.js
+++ index.js
@@ -1,24 +1,17 @@
 const express = require("express");
+const nunjucks = require("nunjucks");
+
 const app = express();
-
-const logMiddleware = (req, res, next) => {
-  console.log(
-    `HOST: ${req.headers.host} | URL: ${req.url} | METHOD: ${req.method}`
-  );
-  req.appName = "GoNode";
-  return next();
-};
-
-app.use(logMiddleware);
-
-app.get("/", (req, res) => {
-  return res.send(`Bem-vindo ao ${req.appName}, ${req.query.name}`);
+nunjucks.configure("views", {
+  autoescape: true,
+  express: app,
+  watch: true
 });
 
-app.get("/nome/:name", (req, res) => {
-  return res.json({
-    message: `Bem-vindo, ${req.params.name}`
-  });
+app.set("view engine", "njk");
+
+app.get("/", (req, res) => {
+  return res.render("list", { name: req.query.name });
 });
 
 app.listen(3000);
